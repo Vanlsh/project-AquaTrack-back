b2768adb9789401138f7d0aab888161a0474d49e
Pavel <Pavel.Pasechnik@gmail.com>
2024-07-12 22:42:52 +0300

    created water swagger

--- src/server.js
+++ src/server.js
@@ -14,14 +14,13 @@ export const setupServer = () => {
 
   app.use(cors());
 
-  app.use('/api-docs', swaggerDocs());
-
   app.use(express.json());
 
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
 
   app.use(router);
+  app.use('/', swaggerDocs());
   app.use(notFoundHandler);
   app.use(errorHandler);
 
