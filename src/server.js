b84cee842879e8eb210ea3d51379ce104818dd3a
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-07-14 18:29:05 +0300

    commit cors setting

--- src/server.js
+++ src/server.js
@@ -8,24 +8,24 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import router from './routers/index.js';
 
-const allowedOrigins = [env(ENV_VARS.APP_DOMAIN), 'http://localhost:5173'];
+// const allowedOrigins = [env(ENV_VARS.APP_DOMAIN), 'http://localhost:5173'];
 
 export const setupServer = () => {
   const PORT = env(ENV_VARS.PORT, '3000');
   const app = express();
 
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  };
+  // const corsOptions = {
+  //   origin: (origin, callback) => {
+  //     if (allowedOrigins.includes(origin) || !origin) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   credentials: true,
+  // };
 
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(cookieParser());
   app.use(express.json());
 
